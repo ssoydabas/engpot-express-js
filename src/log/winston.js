75da71dbc236db8c64cc16bd1c293f09be37563c
Sertan Soydabas <ssoydabas41@gmail.com>
2022-10-28 13:06:35 +0300

    Winston error logging for both computers and humans has been implemented

---
+++ src/log/winston.js
@@ -0,0 +1,31 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, json, errors, printf } = format;
+
+const computerReadableLog = createLogger({
+  format: combine(errors({ stack: true }), timestamp(), json()),
+  transports: [
+    new transports.Console({ level: "info" }),
+    new transports.File({
+      level: "error",
+      filename: "src/log/logs/error.log",
+    }),
+  ],
+});
+
+const humanReadableLog = createLogger({
+  format: combine(
+    timestamp(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.File({ filename: "src/log/logs/error_human.log" }),
+  ],
+});
+
+const logErrors = (error) => {
+  computerReadableLog.error(error);
+  humanReadableLog.error(error);
+};
+
+export default logErrors;
--- src/v1/routes/util/errorHandler.js
+++ src/v1/routes/util/errorHandler.js
@@ -1,5 +1,7 @@
+import logErrors from "../../../log/winston.js";
+
 const errorHandler = (error, req, res, next) => {
-  console.log(error);
+  logErrors(error);
   return res.status(error.statusCode ? error.statusCode : 500).json({
     errorMessage: error.message,
     responseStatus: error.statusCode ? error.statusCode : 500,
